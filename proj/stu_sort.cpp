ec1c31c893166a5770604dd9040665eea0501b88
Sahaj Singh <sahajsingh2313@gmail.com>
2021-10-18 02:50:44 -0700

    still hate strings

--- proj/stu_sort.cpp
+++ proj/stu_sort.cpp
@@ -25,13 +25,27 @@ void SingleSort(Domestic *DomesticStudent, int num, char type)
     {
         for (int j = 0; j < num - 1; j++)
         {
-            if (((type == 'F') && (compareFirstName(DomesticStudent[j], DomesticStudent[j + 1]) > 0)) || ((type == 'L') && (compareLastName(DomesticStudent[j], DomesticStudent[j + 1]) > 0)) || ((type == 'G') && (compareCGPA(DomesticStudent[j], DomesticStudent[j + 1]) > 0)) || ((type == 'R') && (compareResearchScore(DomesticStudent[j], DomesticStudent[j + 1]) > 0)) || ((type == 'P') && (compareProvince(DomesticStudent[j], DomesticStudent[j + 1]))))
+            if (((type == 'F' || type == 'f') && (compareFirstName(DomesticStudent[j], DomesticStudent[j + 1]) > 0)) || ((type == 'L' || type == 'l') && (compareLastName(DomesticStudent[j], DomesticStudent[j + 1]) > 0)) || ((type == 'G' || type == 'g') && (compareCGPA(DomesticStudent[j], DomesticStudent[j + 1]) > 0)) || ((type == 'R' || type == 'r') && (compareResearchScore(DomesticStudent[j], DomesticStudent[j + 1]) > 0)) || ((type == 'P' || type == 'p') && (compareProvince(DomesticStudent[j], DomesticStudent[j + 1]))))
             {
                 Domestic tmpStudent;
                 tmpStudent = DomesticStudent[j];
                 DomesticStudent[j] = DomesticStudent[j + 1];
                 DomesticStudent[j + 1] = tmpStudent;
             }
+        }
+    }
+    if (type == 'G' || type == 'R' || type == 'g' || type == 'r')
+    {
+        int start = 0;
+        int end = num - 1;
+        while (start < end)
+        {
+            Domestic tmpStudent;
+            tmpStudent = DomesticStudent[start];
+            DomesticStudent[start] = DomesticStudent[end];
+            DomesticStudent[end] = tmpStudent;
+            start++;
+            end--;
         }
     }
 }
@@ -42,13 +56,27 @@ void SingleSort(International *InternationalStudent, int num, char type)
     {
         for (int j = 0; j < num - 1; j++)
         {
-            if (((type == 'F') && (compareFirstName(InternationalStudent[j], InternationalStudent[j + 1]) > 0)) || ((type == 'L') && (compareLastName(InternationalStudent[j], InternationalStudent[j + 1]) > 0)) || ((type == 'G') && (compareCGPA(InternationalStudent[j], InternationalStudent[j + 1]) > 0)) || ((type == 'R') && (compareResearchScore(InternationalStudent[j], InternationalStudent[j + 1]) > 0)) || ((type == 'C') && (compareCountry(InternationalStudent[j], InternationalStudent[j + 1]))))
+            if (((type == 'F' || type == 'f') && (compareFirstName(InternationalStudent[j], InternationalStudent[j + 1]) > 0)) || ((type == 'L' || type == 'l') && (compareLastName(InternationalStudent[j], InternationalStudent[j + 1]) > 0)) || ((type == 'G' || type == 'g') && (compareCGPA(InternationalStudent[j], InternationalStudent[j + 1]) > 0)) || ((type == 'R' || type == 'r') && (compareResearchScore(InternationalStudent[j], InternationalStudent[j + 1]) > 0)) || ((type == 'C' || type == 'c') && (compareCountry(InternationalStudent[j], InternationalStudent[j + 1]))))
             {
                 International tmpStudent;
                 tmpStudent = InternationalStudent[j];
                 InternationalStudent[j] = InternationalStudent[j + 1];
                 InternationalStudent[j + 1] = tmpStudent;
             }
+        }
+    }
+    if (type == 'G' || type == 'R' || type == 'g' || type == 'r')
+    {
+        int start = 0;
+        int end = num - 1;
+        while (start < end)
+        {
+            International tmpStudent;
+            tmpStudent = InternationalStudent[start];
+            InternationalStudent[start] = InternationalStudent[end];
+            InternationalStudent[end] = tmpStudent;
+            start++;
+            end--;
         }
     }
 }
