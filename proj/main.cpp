6701aed024b5a0154d65d1d2841a45460e59bcd8
Sahaj Singh <sahajsingh2313@gmail.com>
2021-10-17 23:09:07 -0700

    fix

--- proj/main.cpp
+++ proj/main.cpp
@@ -38,39 +38,35 @@ int main()
   cout << "File format: " << line << endl;
   int i = 0;
   int stu_count = 1;
-  int numDomesticStudents = lineCounter(domesticFile) - 1;
+  int numDomesticStudents = lineCounter(domesticFile);
+  Domestic *DomesticStudents = new Domestic[numDomesticStudents];
   while (getline(domesticFile, line))
   {
     istringstream ss(line);
-    Domestic *DomesticStudents = new Domestic[numDomesticStudents];
     string firstName, lastName, province, s_cgpa, s_researchScore;
     float cgpa;
     int researchScore;
 
-    // get firstName separated by comma
     getline(ss, firstName, ',');
     DomesticStudents[i].setFirstName(firstName);
-    // get lastName separated by comma
     getline(ss, lastName, ',');
     DomesticStudents[i].setLastName(lastName);
-    // get province separated by comma
     getline(ss, province, ',');
     DomesticStudents[i].setProvince(province);
-    // get cpga separated by comma, and convert string to float
     getline(ss, s_cgpa, ',');
     cgpa = atof(s_cgpa.c_str());
     DomesticStudents[i].setCGPA(atof(s_cgpa.c_str()));
-    // get researchScore separated by comma, and convert it to int
     getline(ss, s_researchScore, ',');
     researchScore = atoi(s_researchScore.c_str());
     DomesticStudents[i].setResearchScore(atoi(s_researchScore.c_str()));
 
-    // print the student info to the screen
     cout << "Domestic student " << stu_count << " " << firstName << " "
          << lastName << " from " << province << " province has cgpa of "
          << cgpa << ", and research score of " << researchScore << endl;
 
-    DomesticStudents[i++].setUID(stu_count++);
+    DomesticStudents[i].setUID(stu_count);
+    stu_count++;
+    i++;
   }
 
   ifstream InternationalFile("international-stu.txt");
@@ -86,30 +82,29 @@ int main()
 
   int stu_inter_count = 1;
   int j = 0;
-  int numInternationalStudents = lineCounter(InternationalFile) - 1;
+  int numInternationalStudents = lineCounter(InternationalFile);
+  International *InternationalStudents = new International[numInternationalStudents];
   while (getline(InternationalFile, line))
   {
     istringstream ss(line);
-    International *InternationalStudents = new International[numInternationalStudents];
-    // FirstName,LastName,Country,CGPA,ResearchScore,Reading,Listening,Speaking,Writing
-    string firstName, lastName, country, s_cgpa, s_researchScore, s_reading, s_listening, s_speaking, s_writing;
-    float cgpa;
-    int researchScore, reading, listening, speaking, writing;
+    string firstNameInt, lastNameInt, country, s_cgpa2, s_researchScore2, s_reading, s_listening, s_speaking, s_writing;
+    float cgpa2;
+    int researchScore2, reading, listening, speaking, writing;
 
-    getline(ss, firstName, ',');
-    InternationalStudents[j].setFirstName(firstName);
-    getline(ss, lastName, ',');
-    InternationalStudents[j].setLastName(lastName);
+    getline(ss, firstNameInt, ',');
+    InternationalStudents[j].setFirstName(firstNameInt);
+    getline(ss, lastNameInt, ',');
+    InternationalStudents[j].setLastName(lastNameInt);
     getline(ss, country, ',');
     InternationalStudents[j].setCountry(country);
 
-    getline(ss, s_cgpa, ',');
-    cgpa = atof(s_cgpa.c_str());
-    InternationalStudents[j].setCGPA(atof(s_cgpa.c_str()));
+    getline(ss, s_cgpa2, ',');
+    cgpa2 = atof(s_cgpa2.c_str());
+    InternationalStudents[j].setCGPA(atof(s_cgpa2.c_str()));
 
-    getline(ss, s_researchScore, ',');
-    researchScore = atoi(s_researchScore.c_str());
-    InternationalStudents[j].setResearchScore(atoi(s_researchScore.c_str()));
+    getline(ss, s_researchScore2, ',');
+    researchScore2 = atoi(s_researchScore2.c_str());
+    InternationalStudents[j].setResearchScore(atoi(s_researchScore2.c_str()));
 
     getline(ss, s_reading, ',');
     reading = atoi(s_reading.c_str());
@@ -127,19 +122,24 @@ int main()
     writing = atoi(s_writing.c_str());
     InternationalStudents[j].setWriting(atoi(s_writing.c_str()));
 
-    cout << "International student " << stu_inter_count << " " << firstName << " "
-         << lastName << " from " << country << " has cgpa of "
-         << cgpa << ", and research score of " << researchScore
+    cout << "International student " << stu_inter_count << " " << firstNameInt << " "
+         << lastNameInt << " from " << country << " has cgpa of "
+         << cgpa2 << ", and research score of " << researchScore2
          << " Reading Score: " << reading << " "
          << " Listening Score: " << listening << " "
          << " Speaking Score: " << speaking << " "
          << " Writing Score: " << writing << endl;
 
-    InternationalStudents[j++].setUID(stu_inter_count++);
+    InternationalStudents[j].setUID(numDomesticStudents + stu_inter_count);
+    j++;
+    stu_inter_count++;
   }
+
   // close your file
   domesticFile.close();
+  delete[] DomesticStudents;
   InternationalFile.close();
+  delete[] InternationalStudents;
 
   return 0;
 }
